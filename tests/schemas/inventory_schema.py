2251ba436794383ba809f90903ced61f9bcaf9af
4h5ctgv4xz-arch <4h5ctgv4xz@privaterelay.appleid.com>
2026-02-21 23:03:54 +0300

    add properties in inventory_schema.py

--- tests/schemas/inventory_schema.py
+++ tests/schemas/inventory_schema.py
@@ -1,6 +1,17 @@
 INVENTORY_SCHEMA = {
     "type": "object",
-    "additionalProperties": {
-        "type": "integer"
+    "properties": {
+        "approved": {
+            "type": "integer",
+            "minimum": 0,
+        },
+        "placed": {
+            "type": "integer",
+            "minimum": 0,
+        },
+        "available": {
+            "type": "integer",
+            "minimum": 0,
+        },
     }
 }
